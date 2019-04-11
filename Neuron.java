fcfea2fed9a7f4a23e3f212e082333ac788c0ec5
Russell Coleman <russellsayshi@gmail.com>
2019-04-11 15:45:26 -0500

    initial java push. will be working a lot more as the summer draws nearer for my research assignment

---
+++ Neuron.java
@@ -0,0 +1,17 @@
+public class Neuron {
+	private final int numWeights;
+	private double[] weights;
+
+	public Neuron(numWeights) {
+		this.numWeights = numWeights;
+		weights = new double[numWeights];
+	}
+
+	private double[] getOutputs(double rawInput) {
+		double input = Nonlinearity.
+		double[] out = new double[numWeights];
+		for(int i = 0; i < numWeights; i++) {
+			
+		}
+	}
+}
---
+++ Nonlinearity.java
@@ -0,0 +1,15 @@
+public final class Nonlinearity {
+	private Nonlinearity();
+	public static double normalize(double x) {
+		double pos = Math.exp(x);
+		double neg = Math.exp(-x);
+		double tanh = (pos - neg)/(pos + neg);
+		return tanh;
+	}
+	public static double derivativeAt(double x) {
+		double pos = Math.exp(2*x);
+		double neg = Math.exp(-2*x);
+		double dtanh = 4/(pos + neg + 2);
+		return dtanh;
+	}
+}
